dc040db011c5d4a487680fac0ab11748b3d22dcc
sihala <ssihala@gmail.com>
2024-08-06 19:51:59 -0400

    Finish ADD Instructions

--- src/rustboy.rs
+++ src/rustboy.rs
@@ -105,15 +105,20 @@ impl IndexMut<&str> for CPU_Registers {
 }
 
 pub struct CPU{
-    pub registers: CPU_Registers
+    pub registers: CPU_Registers,
+    pub memory: [u8; 0xFFFF],
+    pub sp: u16
 }
 
 impl CPU{
     pub fn new() -> CPU{
         CPU{
-            registers: CPU_Registers::new()
+            registers: CPU_Registers::new(),
+            memory: [0u8; 0xFFFF],
+            sp: 0
         }
     }
+    //Operand can be any of the 8 bit registers
     pub fn add(&mut self, operand: u8){
         let operand_index = self.registers.index_to_reg(operand);
         let operand_value = self.registers[operand_index];
@@ -124,6 +129,76 @@ impl CPU{
         self.registers.set_flag("HC", ((self.registers["A"] & 0xF) + (operand_value & 0xF)) > 0xF);
         self.registers.set_flag("C", overflow);
         self.registers["A"] = result;
+    }
+
+    pub fn addc(&mut self, operand: u8){
+        let operand_index = self.registers.index_to_reg(operand);
+        let operand_value = self.registers[operand_index];
+        let (result, overflow) = self.registers["A"].overflowing_add(operand_value + self.registers.get_flag("C"));
+        //SET FLAGS
+        self.registers.set_flag("Z", result==0);
+        self.registers.set_flag("S", false);
+        self.registers.set_flag("HC", ((self.registers["A"] & 0xF) + (operand_value & 0xF)) > 0xF);
+        self.registers.set_flag("C", overflow);
+        self.registers["A"] = result;
+    }
+
+    pub fn addc_hl(&mut self){
+        let hl_value = self.registers.get_joint_register("H", "L");
+        let operand_value = self.memory[hl_value as usize];
+        let (result, overflow) = self.registers["A"].overflowing_add(operand_value + self.registers.get_flag("C"));
+        //SET FLAGS
+        self.registers.set_flag("Z", result==0);
+        self.registers.set_flag("S", false);
+        self.registers.set_flag("HC", ((self.registers["A"] & 0xF) + (operand_value & 0xF)) > 0xF);
+        self.registers.set_flag("C", overflow);
+        self.registers["A"] = result;
+    } 
+    pub fn add_n8(&mut self, immediate_val: u8){
+        let (result, overflow) = self.registers["A"].overflowing_add(immediate_val);
+        //SET FLAGS
+        self.registers.set_flag("Z", result==0);
+        self.registers.set_flag("S", false);
+        self.registers.set_flag("HC", ((self.registers["A"] & 0xF) + (immediate_val & 0xF)) > 0xF);
+        self.registers.set_flag("C", overflow);
+        self.registers["A"] = result;
+    }
+    pub fn add_HL_r16(&mut self, operand: u16){
+        let (result, overflow) = self.registers.get_joint_register("H", "L").overflowing_add(operand);
+        self.registers.set_flag("Z", result==0);
+        self.registers.set_flag("S", false);
+        self.registers.set_flag("HC", ((self.registers.get_joint_register("H", "L") & 0xFFF) + (operand & 0xFFF)) > 0xFFF);
+        self.registers.set_flag("C", overflow);
+        self.registers.set_joint_register("H", "L", result);
+    }
+
+    pub fn add_HL_SP(&mut self){
+        let (result, overflow) = self.registers.get_joint_register("H", "L").overflowing_add(self.sp);
+        self.registers.set_flag("Z", result==0);
+        self.registers.set_flag("S", false);
+        self.registers.set_flag("HC", ((self.registers.get_joint_register("H", "L") & 0xFFF) + (self.sp & 0xFFF)) > 0xFFF);
+        self.registers.set_flag("C", overflow);
+        self.registers.set_joint_register("H", "L", result);
+    }
+
+    pub fn add_SP_e8(&mut self, signed_operand: i8){
+        if signed_operand >= 0{
+            let (result, overflow) = self.sp.overflowing_add_signed(signed_operand as i16);
+            self.registers.set_flag("Z", result==0);
+            self.registers.set_flag("HC", ((self.sp & 0xF) as u8 + (signed_operand as u8 & 0xF)) > 0xF);
+            self.registers.set_flag("C", overflow);
+            self.sp = result;
+        }
+        else{
+            let result = self.sp.wrapping_add_signed(signed_operand as i16);
+            self.registers.set_flag("Z", result==0);
+            self.registers.set_flag("HC", (result & 0xF) <= (self.sp & 0xF));
+            self.registers.set_flag("C", (result & 0xFF) <= (self.sp & 0xFF));
+            self.sp = result;
+        }
+        
+        self.registers.set_flag("S", false);
+        
     }
 }
 
@@ -152,10 +227,10 @@ mod tests{
     #[test]
     fn test_add_halfcarry(){
         let mut cpu = super::CPU::new();
-        cpu.registers["A"] = 0b00001111;
+        cpu.registers["A"] = 0b01001111;
         cpu.registers["B"] = 1;
         cpu.add(0);
-        assert_eq!(cpu.registers["A"], 0b10000);
+        assert_eq!(cpu.registers["A"], 0b01010000);
         assert_eq!(cpu.registers["F"], 0b00100000);
     }
     #[test]
@@ -166,5 +241,41 @@ mod tests{
         cpu.add(0);
         assert_eq!(cpu.registers["A"], 0b01111111);
         assert_eq!(cpu.registers["F"], 0b00010000);
+    }
+
+    #[test]
+    fn test_add_hc_16(){
+        let mut cpu = super::CPU::new();
+        cpu.registers.set_joint_register("H", "L", 0b1000111111111111);
+        cpu.add_HL_r16(0b1);
+        assert_eq!(cpu.registers.get_joint_register("H", "L"), 0b1001000000000000);
+        assert_eq!(cpu.registers["F"], 0b00100000);
+    }
+
+    #[test]
+    fn test_add_c_16(){
+        let mut cpu = super::CPU::new();
+        cpu.registers.set_joint_register("H", "L", 0b1111111111111111);
+        cpu.add_HL_r16(0b1);
+        assert_eq!(cpu.registers.get_joint_register("H", "L"), 0b0);
+        assert_eq!(cpu.registers["F"], 0b10110000);
+    }
+
+    #[test]
+    fn test_add_sp_signed1(){
+        let mut cpu = super::CPU::new();
+        cpu.sp = 0b0000000000001111;
+        cpu.add_SP_e8(-15);
+        assert_eq!(cpu.sp, 0b0);
+        assert_eq!(cpu.registers["F"], 0b10110000);
+    }
+
+    #[test]
+    fn test_add_sp_signed2(){
+        let mut cpu = super::CPU::new();
+        cpu.sp = 0b1111;
+        cpu.add_SP_e8(-23);
+        assert_eq!(cpu.sp, 65528);
+        assert_eq!(cpu.registers["F"], 0b00100000);
     }
 }
